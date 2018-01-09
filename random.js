d84b9da64680920282912b47f85e14224c1cd7c5
zioutang <tangziou@gmail.com>
2018-01-08 20:47:10 -0800

    stage 1

--- random.js
+++ random.js
@@ -29,50 +29,7 @@ class Random {
     let index = Math.floor(Math.random() * n);
     return this.list[index];
   }
-  storeOneHundred() { // draft 100 random nums and store in an array. the most recent comes the last.
-    let n = this.numOfValue;
-    while (n > 0) {
-      let num = this.getRandom();
-      this.lastOneHundred.push(num);
-      n--;
-    }
-  }
-  getFreq() { // display each num and its frequency
-    let map = {};
-    this.lastOneHundred.forEach(item => {
-      if (map.hasOwnProperty(item)) {
-        map[item]++
-      } else {
-        map[item] = 1;
-      }
-    })
-    for (let key in map) {
-      map[key] = map[key] / this.lastOneHundred.length;
-    }
-    console.log(map);
-  }
-  getMostRecent() { // get the most recent num and its time stamp
-    return {
-      value: this.lastOneHundred.pop(),
-      time: new Date()
-    }
-  }
-  write(file) { // writing in files in JS is asynchronous
-    let data = this.getMostRecent();
-    jsonfile.writeFile(file, data, err => {
-      // console.log(err);
-    })
-    console.log(`write successfully in the file: ${file} at: `, data.time);
-  }
-  writeInThreads() {
-    let threads = [];
-    let n = 1;
-    while (n <= this.numOfThreads) {
-      threads.push(this.write(`data/thread_${n}.json`));
-      n++;
-    }
-    Promise.all(threads); // using promise to handle asynchronous call to make sure they are called one by one
-  }
+
 
 }
 
